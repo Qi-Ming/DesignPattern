1ffee4f5734b67933373a8e42422cc6fc77b3fb9
cantona <1127276346@qq.com>
2020-08-18 09:28:29 +0800

    [Add] Front Controller Pattern

--- src/FrontControllerPatten/FrontControllerPatternDemo.java
+++ src/FrontControllerPatten/FrontControllerPatternDemo.java
@@ -1,6 +1,7 @@
 package FrontControllerPatten;
 
 public class FrontControllerPatternDemo {
+
     public static void main(String[] args) {
         FrontController controller = new FrontController();
         controller.dispatchRequest("STUDENT");
